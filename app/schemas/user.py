0b61e716b9ca941d646ea855ed1420a930085749
Kostiantyn <kostiaolnev@gmail.com>
2026-06-03 19:59:59 -0700

    feat: add user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr
+
+from datetime import datetime
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    model_config = {
+        'from_attributes': True
+    }
