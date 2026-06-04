ad2a65edc98c67bb1fd9b70660e355a29ddf34d3
Kostiantyn <kostiaolnev@gmail.com>
2026-06-03 20:51:19 -0700

    feat: add JWT utilities

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,13 @@
+from datetime import datetime, timezone, timedelta
+
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+
+from app.core.config import (
+    SECRET_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES
+)
 
 
 pwd_context = CryptContext(
@@ -13,3 +22,34 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> str:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+
+def decode_access_token(token: str) -> str:
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+        return payload
+    
+    except JWTError:
+        return None
