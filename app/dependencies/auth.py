dc913030cf2d68c0f502e2f85063815627b65c9c
Kostiantyn <kostiaolnev@gmail.com>
2026-06-07 12:46:08 -0700

    add current user dependency

---
+++ app/dependencies/auth.py
@@ -0,0 +1,32 @@
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.core.security import decode_access_token
+from app.models.user import User
+
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+        token: str = Depends(oauth_scheme),
+        db: Session = Depends(get_db)
+) -> User:
+    payload = decode_access_token(token)
+
+    if payload is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            status="Invalid or expire token."
+        )
+    
+    user_id = payload.get("sub")
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            status="Invalid token payload."
+        )
+
--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, EmailStr
 
 class LoginRequest(BaseModel):
     email: EmailStr = Field(max_length=255)
-    password: str = Field(min_length=6)
+    password: str = Field(min_length=6, max_length=72)
 
 class TokenResponse(BaseModel):
     access_token: str
