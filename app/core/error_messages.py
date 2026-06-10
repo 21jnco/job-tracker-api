a1f198740e8190d424d3e4032c40fcafc982262b
Kostiantyn <kostiaolnev@gmail.com>
2026-06-10 16:48:03 -0700

    chore: add error messages

---
+++ app/core/error_messages.py
@@ -0,0 +1,22 @@
+# General
+NO_DATA_PROVIDED = "No data provided."
+INVALID_CREDENTIALS = "Invalid credentials."
+UNAUTHORIZED = "Unauthorized."
+FORBIDDEN = "Forbidden."
+
+# User
+USER_NOT_FOUND = "User not found."
+USER_EMAIL_ALREADY_EXISTS = "User with this email already exists."
+PASSWORD_INCORRECT = "Current password is incorrect."
+
+# Job Application
+JOB_APPLICATION_NOT_FOUND = "Job application not found."
+JOB_APPLICATION_NO_DATA_PROVIDED = "No job application data provided."
+
+# Note
+NOTE_NOT_FOUND = "Note not found."
+NOTE_NO_DATA_PROVIDED = "No note data provided."
+
+# Dependencies
+INVALID_OR_EXPIRED_TOKEN = "Invalid or expired token."
+INVALID_TOKEN_PAYLOAD = "Invalid token payload."
