c3d14bb6fe22b8f44e2ed19b661a3b2ffa89f8a4
Kostiantyn <kostiaolnev@gmail.com>
2026-06-14 13:21:10 -0700

    feat: include routers

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ from app.models.job_application import JobApplication
 from app.models.note import Note
 
 from app.api.routers.auth import router as auth_router
+from app.api.routers.job_applications import router as job_application_router
+from app.api.routers.users import router as user_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(job_application_router)
+app.include_router(user_router)
