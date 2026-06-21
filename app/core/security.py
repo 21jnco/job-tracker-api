434e3c3134ebd3e2d908df135679666a12cd2d6b
Kostiantyn <kostiaolnev@gmail.com>
2026-06-20 23:48:44 -0700

    feat: updated create access token function

--- app/core/security.py
+++ app/core/security.py
@@ -24,12 +24,12 @@ def verify_password(plain_password: str, hashed_password: str) -> str:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-    )
+    ))
 
     to_encode.update({"exp": expire})
 
